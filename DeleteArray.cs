e4ad974fa8ea37b9b5ac4fcab0ebcc797b195255
sanjana8100 <sanjanams812@gmail.com>
2023-06-11 22:38:27 +0530

    added: UC1

---
+++ DeleteArray.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DND4_GenericsPrograms
+{
+    internal class DeleteArray
+    {
+        public void DeleteMethod(int[] array)
+        {
+            Console.WriteLine("Type the element that needs to be deleted from the following array: ");
+            for (int index = 0; index < array.Length; index++) Console.WriteLine(array[index]);
+            int element = Convert.ToInt32(Console.ReadLine());
+            int[] newArray = new int[array.Length - 1];
+            int newIndex = 0;
+            for (int index = 0; index < array.Length; index++)
+            {
+                if (array[index] != element)
+                {
+                    newArray[newIndex] = array[index];
+                    newIndex++;
+                }
+            }
+            Console.WriteLine("Array after deleting the element: ");
+            for (int index = 0; index < newArray.Length; index++) Console.WriteLine(newArray[index]);
+        }
+
+        public void DeleteMethod(double[] array)
+        {
+            Console.WriteLine("Type the element that needs to be deleted from the following array: ");
+            for (int index = 0; index < array.Length; index++) Console.WriteLine(array[index]);
+            double element = Convert.ToDouble(Console.ReadLine());
+            double[] newArray = new double[array.Length - 1];
+            int newIndex = 0;
+            for (int index = 0; index < array.Length; index++)
+            {
+                if (array[index] != element)
+                {
+                    newArray[newIndex] = array[index];
+                    newIndex++;
+                }
+            }
+            Console.WriteLine("Array after deleting the element: ");
+            for (int index = 0; index < newArray.Length; index++) Console.WriteLine(newArray[index]);
+        }
+
+        public void DeleteMethod(char[] array)
+        {
+            Console.WriteLine("Type the element that needs to be deleted from the following array: ");
+            for (int index = 0; index < array.Length; index++) Console.WriteLine(array[index]);
+            char element = Convert.ToChar(Console.ReadLine());
+            char[] newArray = new char[array.Length - 1];
+            int newIndex = 0;
+            for (int index = 0; index < array.Length; index++)
+            {
+                if (array[index] != element)
+                {
+                    newArray[newIndex] = array[index];
+                    newIndex++;
+                }
+            }
+            Console.WriteLine("Array after deleting the element: ");
+            for (int index = 0; index < newArray.Length; index++) Console.WriteLine(newArray[index]);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace DND4_GenericsPrograms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] intArray = { 10, 20, 30, 40, 50 };
+            double[] doubleArray = { -5.22, -3.54, 0.88, 2.97, 1.45 };
+            char[] charArray = { 'A', 'b', 'C', 'd', 'E' };
+
+            //Delete element in array for int, double and char
+            DeleteArray deleteArray = new DeleteArray();
+            deleteArray.DeleteMethod(intArray);
+            deleteArray.DeleteMethod(doubleArray);
+            deleteArray.DeleteMethod(charArray);
+
+        }
+    }
+}
